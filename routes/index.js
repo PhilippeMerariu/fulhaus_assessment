cec0c3ba49aea627f3b31941f3f1d5eae0a430e6
PhilippeMerariu <lmerariu@gmail.com>
2022-05-04 20:43:55 -0400

    started acronym route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/acronym', function(req, res, next) {
+  var page = req.query.page;
+  var limit = req.query.limit;
+  var search = req.query.search;
+  res.render('acronym', {lim: limit, srch: search});
+});
+
 module.exports = router;
